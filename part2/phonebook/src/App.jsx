79dc807b7365c06e94f3590cb61a1ccc65126f4d
RHL120 <rhl120@protonmail.com>
2023-12-10 16:27:15 +0100

    Solve 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,13 +5,15 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setnewNumber] = useState('')
   const newNameOnChange = (e) => setNewName(e.target.value)
+  const newNumberOnChange = (e) => setnewNumber(e.target.value)
   const formOnSubmit = (e) => {
     e.preventDefault()
     if (persons.some(x => x.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
     } else {
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName, number: newNumber}))
     }
   }
   return (
@@ -22,11 +24,14 @@ const App = () => {
           name: <input value={newName} onChange={newNameOnChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={newNumberOnChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(x => <p key={x.name}>{x.name}</p>)}
+      {persons.map(x => <p key={x.name}>{x.name} {x.number}</p>)}
     </div>
   )
 }
