8867182d83e31ede1372ef6f62cb8c60e73f5d86
RHL120 <rhl120@protonmail.com>
2023-12-14 21:43:31 +0100

    Solve 1.16 and 1.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import phonebookService from './services/phonebook'
 
-
 const Filter = ({filter, onChange}) =>
   <>
     filter shown with: <input value={filter} onChange={onChange} name='filter'/>
@@ -18,11 +17,15 @@ const PersonForm = ({onSubmit, newName, onChangeNewName, newNumber, onChangeNewN
       <button type='submit'>add</button>
     </div>
   </form>
-const Person = ({person, persons, setPersons}) => {
+const Person = ({person, persons, setPersons, setNotification}) => {
   const onClick = () => {
     if (confirm(`Delete ${person.name} ?`)) {
       phonebookService.deleteContact(person.id)
        .then(() => setPersons(persons.filter(p => p.id !== person.id)))
+       .catch(() => {
+         setNotification({notif: `Information of ${person.name} has already been removed from the server`, isError: true})
+         setTimeout(() => setNotification(null), 5000)
+       })
     }
   }
   return (
@@ -32,15 +35,28 @@ const Person = ({person, persons, setPersons}) => {
     </p>
   )
 }
-const Persons = ({persons, filter, setPersons}) => {
-        return persons
-          .filter(({name}) => name.toLowerCase().includes(filter.toLowerCase()))
-          .map(person => <Person key={person.name} person={person} persons={persons} setPersons={setPersons}/>)}
+
+const Notification = ({notif}) => {
+  console.log(notif);
+  if (notif == null) {
+    return <></>
+  }
+  if (notif.isError) {
+    return <h1 className="error">{notif.notif}</h1>
+  }
+  return <h1 className="succ">{notif.notif}</h1>
+}
+
+const Persons = ({persons, filter, setPersons, setNotification}) => {
+  return persons
+    .filter(({name}) => name.toLowerCase().includes(filter.toLowerCase()))
+    .map(person => <Person key={person.name} person={person} persons={persons} setPersons={setPersons} setNotification={setNotification}/>)}
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState(null)
   useEffect(() => {
     phonebookService.getAll()
       .then((resp) => setPersons(resp))
@@ -59,11 +75,15 @@ const App = () => {
             newPersons[pidx] = newPerson
             setPersons(newPersons)
           })
+        setNotification({notif: `Added ${newName}`, isError: false})
+        setTimeout(() => setNotification(null), 5000)
         setNewNumber('')
         setNewName('')
       }
     } else {
       let newPerson = {name: newName, number: newNumber}
+      setNotification({notif: `Added ${newName}`, isError: false})
+      setTimeout(() => setNotification(null), 5000)
       setNewNumber('')
       setNewName('')
       phonebookService.newContact(newPerson).then(() => setPersons(persons.concat(newPerson)))
@@ -71,12 +91,13 @@ const App = () => {
   }
   return (
     <div>
+      <Notification notif={notification} />
       <Filter filter={filter} onChange={onChange(setFilter)} />
       <h2>Phonebook</h2>
       <h2>Numbers</h2>
       <PersonForm onSubmit={formOnSubmit} newName={newName} onChangeNewName={onChange(setNewName)}
         newNumber={newNumber} onChangeNewNumber={onChange(setNewNumber)} />
-      <Persons persons={persons} filter={filter} setPersons={setPersons}/>
+      <Persons persons={persons} filter={filter} setPersons={setPersons} setNotification={setNotification}/>
     </div>
   )
 }
--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -3,5 +3,5 @@ const URL = 'http://localhost:30001/persons'
 const getAll = () => axios.get(URL).then(resp => resp.data)
 const newContact = new_person => axios.post(URL, new_person)
 const deleteContact = id => axios.delete(`${URL}/${id}`)
-const update = (id, newObject) => axios.put(`${URL}/${id}`, newObject)
+const update = (id, newObject) => {return axios.put(`${URL}/${id}`, newObject)}
 export default { getAll, newContact, deleteContact, update }
