10d06dfb4774b36d5899f14652bd21148cb779f9
RHL120 <rhl120@protonmail.com>
2023-12-15 18:26:30 +0100

    Solve 2.18

---
+++ part2/data_for_countries/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const URL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+
+const matchInsensitive = (str, ...matchers) => matchers.some(matcher => matcher.toLowerCase().includes(str.toLowerCase()))
+
+const CountrySearch = ({val, onChange}) =>
+  <>
+    {"find countries: "}
+    <input value={val} onChange={onChange} />
+  </>
+
+const CountryList = ({countries}) => countries.map(({name}) => <p key={name.official}>{name.common}</p>)
+const CountryFlag = ({flag}) => <p style={{fontSize: "10em", marginTop: 0}}>{flag}</p>
+const DisplayCountry = ({country}) =>
+  <>
+    <h1>{country.name.common}</h1>
+    <p>capital {country.capital[0]}</p>
+    <p>area {country.area}</p>
+    <h3>languages:</h3>
+    <ul>
+      {Object.values(country.languages).map(x => <li key={x}>{x}</li>)}
+    </ul>
+    <CountryFlag flag={country.flag} />
+  </>
+
+function App() {
+  const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+  const [matches, setMatches] = useState([])
+  const onFilterUpdate = (e) => {
+    setFilter(e.target.value)
+    setMatches(countries.filter(country => matchInsensitive(e.target.value, country.name.common, country.name.official)))
+  }
+  useEffect(() => {
+    axios.get(URL)
+      .then(resp => setCountries(resp.data))
+  }, [])
+  return (
+	  <>
+      <CountrySearch val={filter} onChange={onFilterUpdate} />
+      {
+        matches.length <= 10?
+        (
+          matches.length == 1?
+          <DisplayCountry country={matches[0]} /> :
+          <CountryList countries={matches} />
+        ) :
+        <p>Too many matches, specify another filter</p>
+      }
+    </>
+  )
+}
+
+export default App
