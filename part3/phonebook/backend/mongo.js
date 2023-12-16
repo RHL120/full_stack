020e10a09d317a3f3022a21b39ef8af0e65f4580
RHL120 <rhl120@protonmail.com>
2023-12-16 15:36:03 +0100

    Solve 3.12

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const printUsage = () => console.log('Usage: node mongo.js <password> (<name> <number>)?')
+if (process.argv.length < 3) {
+  printUsage()
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://rhl120:${password}@cluster0.mzfhmgr.mongodb.net/?retryWrites=true&w=majority`
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+const Person = mongoose.model('Person', personSchema)
+switch (process.argv.length) {
+  case 3:
+    Person.find({}).then(result => {
+      console.log('phonebook:')
+      result.forEach(({name, number}) => {
+        console.log(name, number)
+      })
+      mongoose.connection.close()
+    })
+    break
+  case 5:
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({
+      name: name,
+      number: number,
+    })
+    person.save().then(result => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+    })
+    break
+  default:
+    printUsage()
+}
